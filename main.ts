bd987af128138c3f14b87da578717482afff1873
Syed Jahanzaib <130786699+Jahanzaibimr@users.noreply.github.com>
2024-03-31 02:09:49 +0500

    Add files via upload
    Typescript Concepts

---
+++ main.ts
@@ -0,0 +1,92 @@
+// let toAge=18;
+
+// let message = toAge >= 18 ? "you are allowed" : "you are not allowed";
+// console.log(`he ${message}`);
+
+// The include more turnuary operators is used first used one by one work ? ,: ,? ,:,?. :
+
+
+// function strenght(){
+//     return "hello my power";
+
+// }
+
+// let myMessage = strenght();
+// console.log(myMessage);
+
+
+// function percentage() {
+//     return 7 % 6
+// }
+
+// let answer = percentage();
+// console.log(answer);
+
+
+// function paper() {
+//     return "Passed";
+// }
+
+// let newpaper = paper();
+
+// if (newpaper === "Failed") {
+//     console.log("You failed the paper.");
+// } else {
+//     console.log("You passed the paper.");
+// }
+
+
+// function sum(num1: number, num2: number){
+//     return num1 + num2 
+// }
+
+// let result = sum(20, 10)
+// console.log(result);
+// *********************************************************
+// function subtract(num1: number, num2: number){
+//     return num1 - num2 
+// }
+
+// let announced = subtract(20, 10)
+// console.log(announced);
+// ***********************************************************
+// function multiply(num1: number, num2: number){
+//     return num1 * num2 
+// }
+
+// let announced = multiply(20, 10)
+// console.log(announced);
+// ************************************************************
+// function divide(num1: number, num2: number){
+//     return num1 / num2 
+// }
+
+// let announced = divide(20, 10)
+// console.log(announced);
+// *************************************************************8
+// function percentage(num1: number, num2: number){
+//     return num1 % num2 
+// }
+
+// let announced = percentage(2, 10)
+// console.log(announced);
+// ****************************************************************
+
+// FUNCTION & PARAMETERS
+
+// function greet(m1: string, m2: string){
+//     return m1 + m2
+// }
+// let message = greet("hello", "brothre");
+// console.log(message)
+
+
+// **********************************************
+
+// //          OPTIONAL VALUE
+
+// function greet(m1: string, m2= "world"){
+//     return m1 + m2
+// }
+// let message = greet("hello");
+// console.log(message)
